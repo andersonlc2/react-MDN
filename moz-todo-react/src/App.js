7958af5bbb91622364a719560646efc4b14d66cf
Anderson Correia <correia@Andersons-iMac.local>
2020-12-30 12:25:31 -0800

    filters

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -1,100 +1,95 @@
+import React, { useState } from 'react';
+import { nanoid } from "nanoid";
+import Todo from './components/Todo';
+import Form from './components/Form';
+import FilterButton from './components/FilterButton';
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+
+// Rendereriza todas as tarefas usando o componente Todo.
 function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
+  
+
+  const taskList = tasks.map((task)=> (
+    <Todo 
+      id={ task.id } 
+      name={ task.name } 
+      completed={ task.completed }
+      key={ task.id }
+      toggleTaskCompleted={ toggleTaskCompleted }
+      deleteTask={ deleteTask }
+      editTask={ editTask }
+    />
+    )
+  );
+
+  // Masca e desmarca o task não só na UI
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      if (id === task.id) {
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  // Delete uma tarefa do array.
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  // Edita uma tarefa.
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+      // Se essa tarefa tem o mesmo id que a editada
+      if (id === task.id){
+        return {...tasks, name: newName}
+        //
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+  
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task'; // Se tiver apenas uma tarefa imprime na tela 'task'.
+  const headingText = `${taskList.length} ${tasksNoun} remaining `; // Mostra quantas tarefas temos.
+
+  // Adiciona uma nova tarefa.
+  function addTask(name) {
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  // Retorno do que o React vai renderizar.
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button> 
-      </form>
+        <Form addTask={ addTask }/>
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show</span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+        <FilterButton btn="All"/>
+        <FilterButton btn="Active"/>
+        <FilterButton btn="Completed"/>
       </div>
-      <h2 id="list-heading">
-        3 tasks remaining
-      </h2>
+      <h2 id="list-heading">{ headingText }</h2>
       <ul
-        role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-        <li className="todo stack-samll">
-          <div className="c-cb">
-            <input id="todo-0" type="checkbox" defaultChecked={true} />
-            <label className="todo-label" htmlFor="todo-0">
-              Eat
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">Eat</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Eat</span>
-            </button>
-          </div>
-        </li>
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-1" type="checkbox" />
-            <label className="todo-label" htmlFor="todo-1">
-              Sleep
-            </label>
-          </div>
-          <div className="btn-group"> 
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">Sleep</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Sleep</span>
-            </button>
-          </div>
-        </li>
-        <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-2" type="checkbox" />
-            <label className="todo-label" htmlFor="todo-2">
-              Repeat
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">Repeat</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Repeat</span>
-            </button>
-          </div>
-        </li>
+        { taskList }
       </ul>
     </div>
   );
 }
 
+// Exporta o componente App para ser usado no index.js
 export default App;
